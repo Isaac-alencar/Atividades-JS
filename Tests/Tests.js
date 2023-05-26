f40480f882ef8c4ff7ad9f7408134e668ddc69b0
jordlolk <jordeallefsilvadecarvalho@gmail.com>
2023-05-26 12:15:32 -0300

    Slider 2.0

--- Tests/Tests.js
+++ Tests/Tests.js
@@ -1,34 +1,34 @@
-/*Next Image button */
+
 const array = ['images/Pratice.jpg', 'images/image2.jpg' ,'images/image3.jpg']
 const arrow_left = document.getElementById('Left')
 const arrow_rigth = document.getElementById('Right')
+//const arrow = document.querySelectorAll('.Arrows') 
 const image = document.getElementById('image')
 let count = 0
-let length = array.length-1
-function moveTitle(){
-   arrow_rigth.addEventListener('click' , ()=>{
-       if(length > count){
-           count++
-           image.src = array[count];
-       }
-       else{
+/* Move images */
+function moveRight(){
+      if(count === array.length-1){
          count = 0
          image.src = array[count]
-       }
-   })
-    arrow_left.addEventListener('click',()=>{
-       if(count > 0){
-          count--
-          image.src = array[count];
-       }
-       else{
-         count = length
+      } else {
+         count++
          image.src = array[count]
-       }
-    })
+      }
    }
- /*End of the Effect*/
- /*left Arrow´s Effect Title*/
+   
+function moveLeft(){
+   
+      if(count === 0){
+         count = array.length-1
+         image.src = array[count]
+      } else {
+         count--
+         image.src = array[count]
+      }
+   }
+ //End of the Effect
+
+ //left Arrow´s Effect Title
  const title = document.getElementById('text')
  arrow_left.addEventListener('mouseenter' , () => {
     title.style.cssText = 'margin-left:5px;'+'transition:0.6s ease-in-out;'
@@ -36,13 +36,12 @@ function moveTitle(){
  arrow_left.addEventListener('mouseleave' , () => {
     title.style.cssText = 'margin-left:5px'+'transition:0.6s ease-in-out;'
  })
-/*End of the Effect*/
-/*Right Arrow´s Effect Title*/
+//End of the Effect
+//Right Arrow´s Effect Title
 arrow_rigth.addEventListener('mouseenter' , () => {
    title.style.cssText = 'margin-right:5px;'+'transition:0.6s ease-in-out;'
 })
 arrow_rigth.addEventListener('mouseleave' , () => {
    title.style.cssText = 'margin-right:5px;'+'transition:0.6s ease-in-out;'
 })
-/*End of the Effect*/
-
+//End of the Effect
